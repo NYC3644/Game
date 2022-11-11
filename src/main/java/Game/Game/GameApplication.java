3a53b033b55ae88ff36debf56d299d276b85da31
NYC <dbscks3644@gmail.com>
2022-11-11 10:52:26 +0900

    first commit

---
+++ src/main/java/Game/Game/GameApplication.java
@@ -0,0 +1,13 @@
+package Game.Game;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameApplication.class, args);
+	}
+
+}
