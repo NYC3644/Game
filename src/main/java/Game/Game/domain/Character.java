0794735274b0dab5bfdf43ef4f676f6d6803f21b
NYC <dbscks3644@gmail.com>
2022-11-11 10:56:08 +0900

    Character domain

---
+++ src/main/java/Game/Game/domain/Character.java
@@ -0,0 +1,59 @@
+package Game.Game.domain;
+
+import java.time.LocalDate;
+
+public class Character {
+    private Long id;
+    private String nickName;
+    private Long level;
+    private Long experience;
+    private LocalDate createdAt;
+
+    public Character(Long id, String nickName, Long level, Long experience, LocalDate createdAt) {
+        this.id = id;
+        this.nickName = nickName;
+        this.level = level;
+        this.experience = experience;
+        this.createdAt = createdAt;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNickName() {
+        return nickName;
+    }
+
+    public void setNickName(String nickName) {
+        this.nickName = nickName;
+    }
+
+    public Long getLevel() {
+        return level;
+    }
+
+    public void setLevel(Long level) {
+        this.level = level;
+    }
+
+    public Long getExperience() {
+        return experience;
+    }
+
+    public void setExperience(Long experience) {
+        this.experience = experience;
+    }
+
+    public LocalDate getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDate createdAt) {
+        this.createdAt = createdAt;
+    }
+}
