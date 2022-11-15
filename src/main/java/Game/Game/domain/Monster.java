500e778f83105b3ea1465a4e86394c59a7c7f586
NYC <dbscks3644@gmail.com>
2022-11-15 16:20:30 +0900

    feat: monster domain

---
+++ src/main/java/Game/Game/domain/Monster.java
@@ -0,0 +1,46 @@
+package Game.Game.domain;
+
+public class Monster {
+
+    private Long id;
+    private String monsterName;
+    private Long experience;
+
+    public Monster(Long id, String monsterName, Long experience) {
+        this.id = id;
+        this.monsterName = monsterName;
+        this.experience = experience;
+
+        monsterNameCheck();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getMonsterName() {
+        return monsterName;
+    }
+
+    public void setMonsterName(String monsterName) {
+        this.monsterName = monsterName;
+    }
+
+    public Long getExperience() {
+        return experience;
+    }
+
+    public void setExperience(Long experience) {
+        this.experience = experience;
+    }
+
+    private void monsterNameCheck() {
+        if(monsterName.length() > 10) {
+            throw new IllegalArgumentException("닉네임은 10글자를 초과할 수 없습니다");
+        }
+    }
+}
