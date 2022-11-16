31fe3956a56b8787496cbd91ff180189df766ab0
NYC <dbscks3644@gmail.com>
2022-11-16 16:53:21 +0900

    feat: 몬스터 검색 중간

--- src/main/java/Game/Game/repository/MonsterMapper.java
+++ src/main/java/Game/Game/repository/MonsterMapper.java
@@ -6,4 +6,6 @@ import org.apache.ibatis.annotations.Mapper;
 @Mapper
 public interface MonsterMapper {
     void save(Monster monster);
+
+    Monster findByMonsterName(String monsterName);
 }
--- src/test/java/Game/Game/service/MonsterServiceTest.java
+++ src/test/java/Game/Game/service/MonsterServiceTest.java
@@ -7,6 +7,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import javax.transaction.Transactional;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
@@ -26,5 +28,19 @@ class MonsterServiceTest {
 
         //then
         assertThat(actual.getId()).isEqualTo(1L);
+    }
+
+    @Test
+    @DisplayName("몬스터 이름으로 검색")
+    void findByMonsterName() {
+        //given
+        Monster monster = new Monster(1L, "Orc", 20L);
+
+        //when
+        Monster actual = monsterService.save(monster);
+        Monster orc = monsterService.findByMonsterName(actual.getMonsterName());
+
+        //then
+        assertThat(orc.getMonsterName()).isEqualTo("Orc");
     }
 }
