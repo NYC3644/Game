e26f0778de452e89e65b1c7e254a98eed23e0566
NYC <dbscks3644@gmail.com>
2022-11-17 15:02:03 +0900

    feat: findById 구현

--- src/main/java/Game/Game/repository/MonsterMapper.java
+++ src/main/java/Game/Game/repository/MonsterMapper.java
@@ -1,11 +1,16 @@
 package Game.Game.repository;
 
 import Game.Game.domain.Monster;
+import Game.Game.domain.User;
 import org.apache.ibatis.annotations.Mapper;
+
+import java.util.Optional;
 
 @Mapper
 public interface MonsterMapper {
     void save(Monster monster);
 
     Monster findByMonsterName(String monsterName);
+
+    Optional<Monster> findById(Long id);
 }
--- src/main/java/Game/Game/repository/UserMapper.java
+++ src/main/java/Game/Game/repository/UserMapper.java
@@ -3,12 +3,16 @@ package Game.Game.repository;
 import Game.Game.domain.User;
 import org.apache.ibatis.annotations.Mapper;
 
+import java.util.Optional;
+
 @Mapper
 public interface UserMapper {
 
     void save(User user);
 
     User findByNickName(String nickName);
+
+    Optional<User> findById(Long id);
 
     User hunt(Long monsterId);
 }
